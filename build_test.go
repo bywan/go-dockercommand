8c4964751a288007633afe429aa0cbe6751e2e5f
Jean-Baptiste Petit <jbpetit@excilys.com>
2016-02-19 09:53:54 +0100

    Be able to pass in BuildOptions the BuildArgs as a map[string]string

--- build_test.go
+++ build_test.go
@@ -21,3 +21,24 @@ func TestDockerBuild(t *testing.T) {
 
 	// TODO clean image
 }
+
+func TestDockerBuildWithBuildArg(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	docker, err := NewDocker("")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	err = docker.Build(&BuildOptions{
+		Dockerfile: "Dockerfile_test",
+		Path:       ".",
+		Tag:        "testgodockercommand",
+		BuildArgs: map[string]string{
+			"HTTP_PROXY": "http://10.20.30.2:1234",
+		},
+	})
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
