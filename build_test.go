f8f35a9f3da96726e8ea2cdaf2f5a74fdef3d210
Jean-Baptiste Petit <jbpetit@excilys.com>
2016-02-19 15:55:41 +0100

    Be able to build without cache

--- build_test.go
+++ build_test.go
@@ -42,3 +42,21 @@ func TestDockerBuildWithBuildArg(t *testing.T) {
 		t.Fatalf("err: %s", err)
 	}
 }
+
+func TestDockerBuildWithoutCache(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	docker, err := NewDocker("")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	err = docker.Build(&BuildOptions{
+		Dockerfile: "Dockerfile_test",
+		Path:       ".",
+		NoCache:    true,
+	})
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
